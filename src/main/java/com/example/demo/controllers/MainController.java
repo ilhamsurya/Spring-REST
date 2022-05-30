b29f88f15e34bf9cac7864a752fd94d274626c64
ilhamsurya <ilhamsurya.m@gmail.com>
2022-05-30 14:57:19 +0700

    refactor folder structure

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,8 +1,8 @@
-package com.example.demo;
+package com.example.demo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -10,7 +10,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/users")
 public class MainController {
 
     private final UserRepository userRepository;
@@ -28,19 +28,22 @@ public class MainController {
     public User getUser(@PathVariable Long id) {
         return userRepository.findById(Math.toIntExact(id)).orElseThrow(RuntimeException::new);
     }
+
     @PostMapping
     public ResponseEntity createUser(@RequestBody User user) throws URISyntaxException {
         User savedUser = userRepository.save(user);
-        return ResponseEntity.created(new URI("/clients/" + savedUser.getId())).body(savedUser);
+        return ResponseEntity.created(new URI("/users/" + savedUser.getId())).body(savedUser);
     }
+
     @PutMapping("/{id}")
     public ResponseEntity updateUser(@PathVariable Long id, @RequestBody User user) {
         User currentUser = userRepository.findById(Math.toIntExact(id)).orElseThrow(RuntimeException::new);
         currentUser.setName(user.getName());
         currentUser.setEmail(user.getEmail());
-
-        return ResponseEntity.ok(currentUser);
+        final User updatedUser = userRepository.save(currentUser);
+        return ResponseEntity.ok(updatedUser);
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity deleteUser(@PathVariable Long id) {
         userRepository.deleteById(Math.toIntExact(id));
--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,7 +1,7 @@
-package com.example.demo;
+package com.example.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.User;
+import com.example.demo.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
